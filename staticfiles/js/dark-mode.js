1d883e4c09966efde067896f706b3933e31537b5
GABRIEL CESAR PEREIRA <gabriel.pereira@rn.local>
2025-12-03 09:06:45 -0300

    Remove staticfiles from tracking

--- staticfiles/js/dark-mode.js
+++
@@ -1,9 +0,0 @@
-// ===== DARK MODE GLOBAL =====
-// Este código deve ser executado em TODAS as páginas
-
-(function() {
-    const darkMode = localStorage.getItem('darkMode') === 'true';
-    if (darkMode) {
-        document.body.classList.add('dark-mode');
-    }
-})();
